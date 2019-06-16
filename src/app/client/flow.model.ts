0d13d138772c79d41abcf77a1bcc76c94557af8d
Rober Veral <roberveral@gmail.com>
2019-06-16 12:17:53 +0200

    [ADD] OAuth 2.0 flow info popover

--- src/app/client/flow.model.ts
+++ src/app/client/flow.model.ts
@@ -8,22 +8,22 @@ export class Flow {
 
     static AuthorizationCode = new Flow(
         "Authorization Code",
-        "Used in user-facing apps and consists in getting an authorization code to exchange for an access token on behalf of the user",
-        "https://www.oauth.com/oauth2-servers/server-side-apps/authorization-code/",
+        "Consists in getting an authorization code after the user gives consent to the client so it can be exchanged for an access token to act on behalf of the user and access OAuth 2.0 Protedted Resources.",
+        "https://www.oauth.com/oauth2-servers/authorization/",
         ["Server-side Web", "Mobile (with PKCE)", "Desktop (with PKCE)", "SPA (with PKCE)"],
         "authorization_code",
         "code");
 
     static Password = new Flow(
         "Password",
-        "Used in first-party native apps and consists in using the user credentials introduced in the application to obtain an access token on behalf of the user",
+        "Consists in obtaining the end-user credentials and exchange them directly for an access token. Given that the user has to introduce his credentials in the client it MUST only be used by first-party apps.",
         "https://www.oauth.com/oauth2-servers/access-tokens/password-grant/",
-        ["Native & First-party"],
+        ["First-party native apps"],
         "password");
 
     static ClientCredentials = new Flow(
         "Client Credentials",
-        "Client uses its credentials to obtain an access token to act on its own behalf",
+        "Client uses its credentials to obtain an access token to act on its own behalf and access its own resources. No end-user involved.",
         "https://www.oauth.com/oauth2-servers/access-tokens/client-credentials/",
         ["Service-to-service"],
         "client_credentials");
